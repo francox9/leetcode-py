85bac6c4b6d080b11cce5d827dd5f5ca2e743df3
francox9 <sunxufei92@gmail.com>
2020-04-11 14:54:30 +1000

    Valid parenthese

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -11,6 +11,17 @@ class Solution:
         >>> Solution.isValid(Solution, "(]")
         False
         '''
+        stack = []
+        leftParentheses = {'(', '[', '{'}
+        pairs = {'()', '[]', '{}'}
+        for ch in s:
+            if ch in leftParentheses:
+                stack.append(ch)
+            else:
+                if len(stack) < 1 or (stack.pop() + ch) not in pairs:
+                    return False
+
+        return len(stack) is 0
 
 
 if __name__ == "__main__":
